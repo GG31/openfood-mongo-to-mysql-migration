bf827f07f0272fc78905a158db095a94c8eb8673
Geneviève Cirera <gcirera1@ypg.com>
2018-07-16 19:55:05 -0400

    Tests

--- src/migration/migration_test.py
+++ src/migration/migration_test.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock, call
 from .migration import Migration
-from .models import Category, Product
+from .models import Category, Country, Brand
 
 import config
 
@@ -54,5 +54,57 @@ def test_migrate_with_product_categories(mongo_db, mysql_client):
     call2 = call(Category, tag='cat2', name='cat2')
     mysql_client.get_or_create.assert_has_calls([call1, call2])
     mysql_client.commit.assert_called_once()
-    # mysql_client.insert.assert_called_once_with(Product(name='test1', language='fr', barcode='code_test', category=[Category(tag='cat1', name='cat1'), Category(tag='cat2', name='cat2')]))
-# TODO tests
+
+
+products = [{
+    'product_name': 'test1',
+    'code': 'code_test',
+    'lang': 'fr',
+    'origins': 'origin1,origin2',
+    'origins_tags': ['or1', 'or2']
+}]
+@pytest.mark.parametrize('mongo_db', [(products)], indirect=['mongo_db'])
+def test_migrate_with_product_country_origin(mongo_db, mysql_client):
+    migration = Migration(test_config, MagicMock(), mongo_db, mysql_client)
+    migration.migrate()
+    mongo_db['test'].find.assert_called_once()
+    call1 = call(Country, tag='or1', name='origin1')
+    call2 = call(Country, tag='or2', name='origin2')
+    mysql_client.get_or_create.assert_has_calls([call1, call2])
+    mysql_client.commit.assert_called_once()
+
+
+products = [{
+    'product_name': 'test1',
+    'code': 'code_test',
+    'lang': 'fr',
+    'countries': 'country1,country2',
+    'countries_tags': ['c1', 'c2']
+}]
+@pytest.mark.parametrize('mongo_db', [(products)], indirect=['mongo_db'])
+def test_migrate_with_product_country_selling(mongo_db, mysql_client):
+    migration = Migration(test_config, MagicMock(), mongo_db, mysql_client)
+    migration.migrate()
+    mongo_db['test'].find.assert_called_once()
+    call1 = call(Country, tag='c1', name='country1')
+    call2 = call(Country, tag='c2', name='country2')
+    mysql_client.get_or_create.assert_has_calls([call1, call2])
+    mysql_client.commit.assert_called_once()
+
+
+products = [{
+    'product_name': 'test1',
+    'code': 'code_test',
+    'lang': 'fr',
+    'brands': 'brand1,brand2',
+    'brands_tags': ['b1', 'b2']
+}]
+@pytest.mark.parametrize('mongo_db', [(products)], indirect=['mongo_db'])
+def test_migrate_with_product_brand(mongo_db, mysql_client):
+    migration = Migration(test_config, MagicMock(), mongo_db, mysql_client)
+    migration.migrate()
+    mongo_db['test'].find.assert_called_once()
+    call1 = call(Brand, tag='b1', name='brand1')
+    call2 = call(Brand, tag='b2', name='brand2')
+    mysql_client.get_or_create.assert_has_calls([call1, call2])
+    mysql_client.commit.assert_called_once()
